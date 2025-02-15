88455e36217132023b7a5d35fd391607c5308380
chamit6450 <chamit6450@gmail.com>
2025-02-15 13:32:54 +0530

    Get route fix

--- app/api/tests/[id]/route.ts
+++ app/api/tests/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request, { params }: { params: { id?: string } }) {
+export async function GET(request: NextRequest,response: NextResponse, { params }: { params: { id?: string } }) {
   const id = params.id;
 
   if (!id) {
